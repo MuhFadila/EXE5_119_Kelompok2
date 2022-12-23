3e6b2a9eb1fbae7a046e863e5b11d9960b572673
Muhammad Fadila <muh.fadila18@gmail.com>
2022-12-23 16:09:09 +0700

    delete wrong code

--- Program.cs
+++ Program.cs
@@ -12,20 +12,4 @@ namespace EXE5_119_Kelompok2
             this.next = null;
         }
     }
-    class Queue
-    {
-        public QNode front, rear;
-        public Queue() { this.front = this.rear = null; }
-        public void enqueue(int key)
-        {
-            QNode temp = new QNode(key);
-            if (this.rear == null)
-            {
-                this.front = this.rear = temp;
-                return;
-            }
-            this.rear.next = temp;
-            this.rear = temp;
-        }
-    }
 }
